793fa95be47266102fae4169c03b3577cebcad4e
zeo <zeo29021992@gmail.com>
2018-06-02 01:21:17 +0530

    Working printk with pmm

---
+++ part11-physical-mem-manager/drivers/printk.h
@@ -0,0 +1,4 @@
+#ifndef PRINTK_H
+#define PRINTK_H
+void printk (const char *fmt, ...);
+#endif
--- part11-physical-mem-manager/drivers/screen.h
+++ part11-physical-mem-manager/drivers/screen.h
@@ -8,7 +8,19 @@
 #define REG_SCREEN_CTRL 0x3d4
 #define REG_SCREEN_DATA 0x3d5
 
+// unsigned short *video_memory = (unsigned short *)0xB8000;
+// // Stores the cursor position.
+// unsigned char cursor_x = 0;
+// unsigned char cursor_y = 0;
+
 /* Public kernel API */
 void clear_screen();
 void kprint_at(char *message, int col, int row);
 void kprint(char *message);
+void monitor_put(char c);
+
+// Clear the screen to all black.
+void monitor_clear();
+
+// Output a null-terminated ASCII string to the monitor.
+void monitor_write(char *c);
