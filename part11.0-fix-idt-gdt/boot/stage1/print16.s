3d1e89daf2202f3bffa29fefbf932c20bbc53664
zeo <zeo29021992@gmail.com>
2018-06-02 01:26:19 +0530

    Remove staging code

--- part11.0-fix-idt-gdt/boot/stage1/print16.s
+++
@@ -1,9 +0,0 @@
-Print:
-	lodsb						; load next byte from string from SI to AL
-	or	al, al					; Does AL=0?
-	jz	PrintDone				; Yep, null terminator found-bail out
-	mov	ah, 0eh					; Nope-Print the character
-	int	10h
-	jmp	Print					; Repeat until null terminator found
-PrintDone:
-	ret						; we are done, so return
--- part11.0-fix-idt-gdt/drivers/vga.h
+++
@@ -1,4 +0,0 @@
-void vgaGraphicsModeInit();
-unsigned char *getFrameBufferSegment();
-unsigned char getColorIndex(unsigned char r, unsigned char g, unsigned char b);
-void fillRectangle(unsigned int x, unsigned int y, unsigned int w, unsigned int h, unsigned char r, unsigned char g, unsigned char b);
